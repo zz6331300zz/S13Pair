f7151bc11a3414cdcac0dec095b25a4e38554efd
zz6331300zz <zz6331300zz@gmail.com>
2024-06-21 12:13:36 +0900

    ex1~12까지 커밋

--- S13/A/S13A/src/Pair1.java
+++ S13/A/S13A/src/Pair1.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Pair1 {
+
+    public static void main(String[] args) {
+
+        int n = 10;
+
+        String[] strArr = String.valueOf(n).split("");
+        int sum = 0;
+        for(int i=0;i<strArr.length;i++){
+            sum += Integer.parseInt(strArr[i]);
+        }
+        boolean answer = false;
+        if(n%sum==0){
+            answer = true;
+        }
+        System.out.println(answer);
+    }
+
 }
